6232a3b63f93454203d0342863449823ec6a9a16
yxj0312 <yanxiajun@hotmail.com>
2022-03-28 23:24:02 +0200

    add nuxtlink

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <nav>Nav goes here</nav>
+    <nav>
+        <ul>
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+            <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+        </ul>
+    </nav>
     <slot />
   </div>
 </template>
