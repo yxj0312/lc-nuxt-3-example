dc0baedd9038a4b900fbd9392fb9823734cd214e
yxj0312 <yanxiajun@hotmail.com>
2022-03-30 21:50:57 +0200

    fetch data, render on the client

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,27 @@
         <div>This is the main page</div>
         <Counter />
     </div>
-    x: {{ x }}
-    y: {{ y }}
+    <div>
+        x: {{ x }}
+        y: {{ y }}
+    </div>
+    <div>
+        <ul>
+            <li v-for="user in users" :key="user.id">
+                {{ user.name }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script setup>
     const {x,y} = useMouse()
+
+    const users = ref([])
+
+    onMounted(()=> {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response =>  response.json())
+        .then(data =>  users.value = data)
+    }) 
 </script>
