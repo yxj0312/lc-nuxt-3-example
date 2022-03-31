affdfa7fc3d903e88ff4a4613493fad16a607264
yxj0312 <yanxiajun@hotmail.com>
2022-03-31 22:45:42 +0200

    add title

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,6 +14,7 @@
 
 <template>
   <div class="bg-gray-300 min-h-screen text-gray-900">
+    <Title>{{ title }}</Title>
     <nav class="bg-white shadow text-lg px-6 py-6">
       <div class="container mx-auto flex items-center justify-between px-6">
         <div>
@@ -21,9 +22,15 @@
         </div>
         <div>
           <ul class="flex space-x-12">
-            <li><NuxtLink to="/">Home</NuxtLink></li>
-            <li><NuxtLink to="/about">About</NuxtLink></li>
-            <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+            <li>
+              <NuxtLink to="/">Home</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/about">About</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/contact">Contact</NuxtLink>
+            </li>
           </ul>
         </div>
       </div>
@@ -32,8 +39,12 @@
   </div>
 </template>
 
+<script setup>
+const title = 'Nuxt 3 Blog'
+</script>
+
 <style>
-  .router-link-active {
-    font-weight: bold;
-  }
+.router-link-active {
+  font-weight: bold;
+}
 </style>
