bc82f017f4d40dd8acaf593ccfc06f78ea4bec57
yxj0312 <yanxiajun@hotmail.com>
2022-04-18 23:06:52 +0200

    add title  to post show

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,7 +1,8 @@
 <template>
     <div class="container mx-auto w-1/2 py-8">
+        <Title>{{ post.title }} |  {{ title }}</Title>
         <h3 class="text-3xl font-bold">
-            <NuxtLink :to="`/posts/${post.id}`">{{post.title}}</NuxtLink>
+            <NuxtLink :to="`/posts/${post.id}`">{{ post.title }}</NuxtLink>
         </h3>
         <div class="post-meta text-gray-700 flex items-center space-x-2">
             <div>{{post.created}}</div>
